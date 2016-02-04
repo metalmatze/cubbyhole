6b469a5f9848e663229cc536f32c160f64e0274d
Matthias Loibl <mail@matthiasloibl.com>
2016-02-04 20:10:18 +0100

    Handle requests and return simple responses

--- server/main.go
+++ server/main.go
@@ -8,6 +8,20 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
+)
+
+const (
+	RESPONSE_WELCOME       = "!HELLO: Welcome to the Cubbyhole Server! Try 'help' for a list of commands"
+	RESPONSE_HELP          = "!HELP:\nThe following commands are supported by this Cubbyhole:\n\nPUT <message>\t- Places a new message in the cubbyhole\nGET\t\t- Takes the message out of the cubbyhole and displays it\nLOOK\t\t- Displays the massage without taking it out of the cubbyhole\nDROP\t\t- Takes the message out of the cubbyhole without displaying it\nHELP\t\t- Displays this help message\nQUIT\t\t- Terminates the connection\n"
+	RESPONSE_DROP          = "!DROP: ok"
+	RESPONSE_GET           = "!GET: "
+	RESPONSE_LOOK          = "!LOOK: "
+	RESPONSE_PUT           = "!PUT: ok"
+	RESPONSE_QUIT          = "!QUIT: ok"
+	RESPONSE_NOT_SUPPORTED = "!NOT SUPPORTED"
+	RESPONSE_NO_MESSAGE    = "<no message stored>"
+	RESPONSE_PROPMT        = "\n> "
 )
 
 var (
@@ -41,6 +55,8 @@ func main() {
 
 			go handleRequest(connection, channel, errChannel)
 			go sendData(connection, channel)
+
+			channel <- RESPONSE_WELCOME + RESPONSE_PROPMT
 		}
 	}
 
@@ -56,7 +72,24 @@ func handleRequest(connection net.Conn, channel chan string, errChannel chan err
 		}
 
 		request := string(buffer[:len])
-		channel <- request
+		requestStrings := strings.Split(strings.TrimSpace(request), " ")
+
+		switch strings.ToLower(requestStrings[0]) {
+		case "put":
+			channel <- "putting"
+		case "get":
+			channel <- "getting"
+		case "look":
+			channel <- "looking"
+		case "drop":
+			channel <- "dropping"
+		case "help":
+			channel <- RESPONSE_HELP + RESPONSE_PROPMT
+		case "quit":
+			connection.Close()
+		default:
+			channel <- RESPONSE_NOT_SUPPORTED + RESPONSE_PROPMT
+		}
 	}
 }
 
