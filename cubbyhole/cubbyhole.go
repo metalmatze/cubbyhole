7ed2dcadd050e6e3fb165fc82c8b926cf1eecb6a
Matthias Loibl <mail@matthiasloibl.com>
2016-02-23 00:15:48 +0100

    Add comments to cubbyhole struct and its funcs

--- cubbyhole/cubbyhole.go
+++ cubbyhole/cubbyhole.go
@@ -1,21 +1,26 @@
 package cubbyhole
 
+// Cubbyhole is a secret place where you can put a message
 type Cubbyhole struct {
 	Message string
 }
 
+// Put places your message into the cubbyhole
 func (c *Cubbyhole) Put(message string) {
 	c.Message = message
 }
 
+// Drop removes your message without returning it first
 func (c *Cubbyhole) Drop() {
 	c.Message = ""
 }
 
+// Look gets the message
 func (c *Cubbyhole) Look() string {
 	return c.Message
 }
 
+// Get returns your message and then removes it
 func (c *Cubbyhole) Get() string {
 	message := c.Look()
 	c.Drop()
